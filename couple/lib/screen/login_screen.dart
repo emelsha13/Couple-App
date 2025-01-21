81232137d6f79d9ace432edb60782f609d629970
emelsha viadra <emelsha.viadra@mhs.itenas.ac.id>
2025-01-21 15:24:25 +0700

    1

--- couple/lib/screen/login_screen.dart
+++ couple/lib/screen/login_screen.dart
@@ -5,7 +5,55 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build (BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF01321F),
+      body: SafeArea (
+        child: Center(
+        child: Column (
+          children:const [
+             SizedBox(height: 240),
+
+            //logo
+               Icon(
+                Icons.lock_outline,
+                size: 80,
+                color: Color(0xFFCDCDC3), 
+              ),
+
+              SizedBox(height: 50),
+
+            //welcome back
+              Text(
+                "Hi Dear!",
+                style: TextStyle(
+                  color: Color(0xFFCDCDC3),
+                  fontSize: 30,
+                  height: 1,
+                  
+
+
+
+                ),
+                ),
+            //username
+
+            //password
+
+            //forgot
+
+            //sign
+
+            //or continue with
+
+            //google apple sign
+
+            //not a member? go register
+
+            ]
+        )
+      )
+      )
+    );
 
   }
 }
